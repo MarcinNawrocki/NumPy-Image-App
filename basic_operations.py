cfd1a3759adcbdae3b3b95498b5f9a23894c4044
maksym <marcinn580@gmail.com>
2020-04-10 15:25:20 +0200

    First complete version of basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -3,45 +3,147 @@
 import time
 import numpy as np 
 from PIL import Image
-
-start = time.time()
-#odczyt obrazu:
-image = Image.open("obraz.bmp")
-#podstawowe informacje o obrazie
-print(image.format)
-print(image.size)
-print(image.mode)
-
-#przejście do NumPy array
-data = np.array(image, dtype=np.uint8)
-data.setflags(write=1)
-print(type(data))
-print(data.shape)
-
-to_mono_vector = [0.2125 , 0.7154 , 0.0721 ]  #human grayscale, for machine (R+G+B)/
-grayscale = np.zeros((data.shape[0],data.shape[1]), dtype=np.uint8)
-grayscale = np.around(to_mono_vector[0]*data[:,:,0] + to_mono_vector[1]*data[:,:,1] + to_mono_vector[2]*data[:,:,2])
-grayscale_machine = np.around(np.mean(data, axis=2))
+import matplotlib.pyplot as plt
 
 
-grayscale = grayscale.astype(np.uint8)
-grayscale_machine = grayscale_machine.astype(np.uint8)
+#dla obu operacji odczytu zapisu trzeba zrobić obsługę błędów
+def readImage(filename, verbose=False):
+    """Reading image from file and transform it to NumPy array
+
+    Keyword argument:
+    filename -- relative path to image
+    verbose -- if true showing image(default False)
+
+    Return:
+    image as Numpy Arra
+    """
+
+    pil_image = Image.open(filename)
+    if verbose:
+        pil_image.show()
+    np_image = np.array(pil_image, dtype=np.uint8)
+    np_image.setflags(write=1)
+    return np_image
+
+def saveImage(np_image, filename, verbose=False):
+    """Saving image to file
+
+    Keyword argument:
+    np_image -- image as NumPy array
+    filename -- relative to where image should be saved
+    verbose -- if true showing image(default False)
+
+    Return:
+    #TODO Code
+    """
+
+    #tutaj w zależności jest czy obraz czarno biały czy kolorowy różne
+    #image2 = Image.fromarray(grayscale, mode ='L')
+    mode = getImageColorType(np_image)
+    pil_image = Image.fromarray(np_image, mode=mode)
+    if verbose:
+        pil_image.show()
+    pil_image.save(filename)
+    #return coś o powodzeniu operacji
+
+def getHumanGrayscale(np_image):
+    """Convert image to "Human" grayscale (0,215*R+0.7151*G+0.0721*B)
+
+    Keyword argument:
+    np_image -- image as NumPy array
+    Return:
+    np_image_gray -- image as grayscale
+    Important:
+    !!! This operation reducing Array dimension from 3 to 2 !!!
+    """
+
+    to_mono_vector = [0.2125 , 0.7154 , 0.0721 ]
+    np_image_gray = np.zeros((np_image.shape[0],np_image.shape[1]), dtype=np.uint8)
+    np_image_gray = np.around(to_mono_vector[0]*np_image[:,:,0] + to_mono_vector[1]*np_image[:,:,1] + to_mono_vector[2]*data[:,:,2])
+    np_image_gray = np_image_gray.astype(np.uint8)
+    return np_image_gray
+
+def getMachineGrayscale(np_image):
+
+    """Convert image to "Machine" grayscale (R+G+B)/3
+
+    Keyword argument:
+    np_image -- image as NumPy array
+    Return:
+    np_image_gray -- image as grayscale
+    Important:
+    !!! This operation reducing Array dimension from 3 to 2 !!!
+    """
+
+    np_image_gray = np.zeros((np_image.shape[0],np_image.shape[1]), dtype=np.uint8)
+    np_image_gray = np.around(np.mean(np_image, axis=2))
+    np_image_gray = np_image_gray.astype(np.uint8)
+    return np_image_gray
+
+def getImageColorType(np_image):
+    """Return image mode
+
+    Keyword argument:
+    np_image -- image as NumPy array
+    Return:
+    String data:
+        L for grayscale image
+        RGB for color image
+    """
+
+    if len(np_image.shape) == 3:
+        return 'RGB'
+    elif len(np_image.shape) == 2:
+        return 'L'
+    #tutaj jakaś obsługa błędów
+
+def getBasicImageParameters(pil_image):
+    """Return basic image parameters as dictionary with info about Resolution, format and mode
+
+    Keyword argument:
+    pil_image -- image as PIL object
+    Return:
+        Python dictionary with keys: Resolution, Format, Mode 
+    """
+
+    return {"Resolution" : image.size, "Format": image.format, 'Mode': image.mode}
+
+def getMinMaxPix(np_image_2dim):
+    """Return  dictionary with max and min pixel value
+
+    Keyword argument:
+    np_image -- image as 2D NumPy array(whole grayscale or one color channel)
+    Return:
+        Python dictionary with keys: Max value, Min value 
+    """
+
+    return {"Max value" : np.amax(np_image_2dim), "Min value" : np.amin(np_image_2dmin)}
+
+def getStatisticImageParameters(np_image_2dim):
+    """Return statistical image parameters as dictionary(Variance, Standard devation, Median, Average)
+
+    Keyword argument:
+    np_image -- image as 2D NumPy array(whole grayscale or one color channel)
+    Return:
+        Python dictionary with keys: Variance, Standard devation, Median, Average 
+    """
+
+    return {'Variance' : np.var(np_image_2dim), 'Standard devation' : np.std(np_image_2dim), "Median" : np.median(np_image_2dim), "Average" : np.average(np_image_2dim)}
+
+def getImageHistogram(np_image_2dim):
+    """
+    Return histogram for image in 2D Numpy array(grayscale or single channel)
+    """
+    return np.histogram(np_image_2dim, bins=range(257))[0]
+
+
+
+data = readImage("obraz.bmp", verbose=False)
+data_gray = getHumanGrayscale(data)
+data_rgb = getRGB(data_gray)
+saveImage(data_rgb, "RGB.bmp", verbose=True)
+
 #pętla przez wszystkie piksele
-for xy in np.ndindex(data.shape[:2]):
-    print(str(grayscale[xy])+", " + str(grayscale_machine[xy]))
-    time.sleep(0.3)
-    
+#for xy in np.ndindex(data.shape[:2]):
+  # print(str(data[xy])+", " + str(data[xy]))
 
-#powrót do Pillow image
-image2 = Image.fromarray(grayscale, mode ='L')
-image3 = Image.fromarray(grayscale_machine, mode='L')
-#image2 = Image.fromarray(datas)
-image2.show(title="human")
-image3.show(title="machine")
-print("image2 parameters:")
-print(image2.format)
-print(image2.size)
-print(image2.mode)
-image2.save("grayscale.bmp")
-end = time.time()
-print(end - start)
